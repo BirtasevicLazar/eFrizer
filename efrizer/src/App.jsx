5d96b505e67b65bbbf73f43ee28f09688fc1379e
BirtasevicLazar <lazar.birtasevic1@gmail.com>
2024-11-09 16:17:33 +0100

    HeroComponent

--- efrizer/src/App.jsx
+++ efrizer/src/App.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import './App.css';
 import NavigationBar from './Components/Navbar/Navbar.jsx';
 import Footer from './Components/Footer/Footer.jsx';
+import Hero from './Components/Hero/Hero.jsx';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <div className="App-hero">
-        <header className="App-header">
-          <h1>Finansijska infrastruktura za rast vaših prihoda</h1>
-          <p>
-            Pridružite se milionima kompanija svih veličina koje koriste Stripe za prihvatanje plaćanja online i uživo, ugrađivanje finansijskih usluga, pokretanje prilagođenih modela prihoda i izgradnju profitabilnijeg poslovanja.
-          </p>
-        </header>
-      </div>
+      <Hero />
       <Footer />
     </div>
   );
---
+++ efrizer/src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <div className="App-hero">
+      <header className="App-header">
+        <h1>Finansijska infrastruktura za rast vaših prihoda</h1>
+        <p>
+          Pridružite se milionima kompanija svih veličina koje koriste Stripe za prihvatanje plaćanja online i uživo, ugrađivanje finansijskih usluga, pokretanje prilagođenih modela prihoda i izgradnju profitabilnijeg poslovanja.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Hero; 
