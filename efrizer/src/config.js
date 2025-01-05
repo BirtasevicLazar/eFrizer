4c70cd3af18a210901c5ff667ef01610a724cb51
BirtasevicLazar <lazar.birtasevic1@gmail.com>
2025-01-05 03:21:28 +0100

    ip

--- efrizer/src/config.js
+++ efrizer/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.0.27:8888/efrizer/php_api';
-export const API_URL2 = 'http://192.168.0.27:5173/';
+export const API_BASE_URL = 'http://192.168.0.28:8888/efrizer/php_api';
+export const API_URL2 = 'http://192.168.0.28:5174/';
--- php_api/cors.php
+++ php_api/cors.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://192.168.0.27:5173');
+header('Access-Control-Allow-Origin: http://192.168.0.28:5174');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true');
